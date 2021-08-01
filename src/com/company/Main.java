35b0635468e0a6ab5f9b718520eadfba93b73da3
wasp <wasporc@mail.ru>
2021-08-02 00:00:28 +0300

    Person Builder

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,17 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Person person = new Person.Builder()
+                .age(10)
+                .address("address")
+                .country("country")
+                .lastName("Last Name")
+                .middleName("Middle Name")
+                .gender("Robot")
+                .phone("Phone")
+                .firstName("First Name")
+                .build();
+        System.out.println(person);
+
     }
 }
---
+++ src/com/company/Person.java
@@ -0,0 +1,95 @@
+package com.company;
+
+public class Person {
+    private final String firstName;
+    private final String lastName;
+    private final String middleName;
+    private final String country;
+    private final String address;
+    private final String phone;
+    private final int age;
+    private final String gender;
+
+    private Person(Builder builder) {
+        this.firstName = builder.firstName;
+        this.lastName = builder.lastName;
+        this.middleName = builder.middleName;
+        this.country = builder.country;
+        this.address = builder.address;
+        this.phone = builder.phone;
+        this.age = builder.age;
+        this.gender = builder.gender;
+    }
+
+    public static class Builder {
+        private String firstName;
+        private String lastName;
+        private String middleName;
+        private String country;
+        private String address;
+        private String phone;
+        private int age;
+        private String gender;
+
+        public Builder() {
+        }
+
+        public Builder firstName(String firstName){
+            this.firstName = firstName;
+            return this;
+        }
+
+        public Builder lastName(String lastName){
+            this.lastName = lastName;
+            return this;
+        }
+
+        public Builder middleName(String middleName){
+            this.middleName = middleName;
+            return this;
+        }
+
+        public Builder country(String country){
+            this.country = country;
+            return this;
+        }
+
+        public Builder address(String address){
+            this.address = address;
+            return this;
+        }
+
+        public Builder phone(String phone){
+            this.phone = phone;
+            return this;
+        }
+
+        public Builder gender(String gender){
+            this.gender = gender;
+            return this;
+        }
+
+        public Builder age(int age){
+            this.age = age;
+            return this;
+        }
+
+        public Person build(){
+            return new Person(this);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", middleName='" + middleName + '\'' +
+                ", country='" + country + '\'' +
+                ", address='" + address + '\'' +
+                ", phone='" + phone + '\'' +
+                ", age=" + age +
+                ", gender='" + gender + '\'' +
+                '}';
+    }
+}
